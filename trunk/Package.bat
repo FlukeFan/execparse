05b3c39c75b1f4c714fb81e4d3da122c320ee787
FlukeFan <FlukeFan@3da37478-0247-0410-bd38-07bdd6096719>
2008-06-05 12:17:53 +0000

    Added debug messages.

--- trunk/Package.bat
+++ trunk/Package.bat
@@ -1,5 +1,5 @@
 
-SET PackageFolder=ExecParse1.0.0.0
+SET PackageFolder=ExecParse1.1.0.0
 
 C:\WINDOWS\Microsoft.NET\Framework\v2.0.50727\MSBuild /p:Configuration=Release /t:Clean
 C:\WINDOWS\Microsoft.NET\Framework\v2.0.50727\MSBuild /p:Configuration=Release /t:Build
